ec9ff20408e002f574c0c23b3dd179e7c2e33790
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-03-30 08:05:20 +0100

    01.sh and 02.sh

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,4 @@
 # Create a wallet with the name "btrustwallet".
+
+WALLET=$(bitcoin-cli -regtest -named createwallet wallet_name="btrustwallet")
+echo "$WALLET"
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,11 @@
 # Create a native segwit address and get the public key from the address.
+
+# Generate a native SegWit (Bech32) address
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" bech32)
+
+# Get the public key associated with the address
+PUBKEY=$(bitcoin-cli -regtest getaddressinfo "$ADDRESS" | jq -r '.pubkey')
+
+# Output results
+echo "$ADDRESS"
+echo "$PUBKEY"
