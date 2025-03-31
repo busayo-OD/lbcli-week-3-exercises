cba2d2bc8a2baa0fe94df319dc6f7c37f68d6a26
Busayo Dada <toyinoluwabusayo@gmail.com>
2025-03-31 22:31:14 +0100

    06.sh

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,17 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+# Given partially signed transaction
+PSBT="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+# Decode PSBT and capture the output
+output=$(bitcoin-cli -regtest -named decodepsbt psbt=$PSBT)
+
+# Extract txid and hash from the output
+txid=$(echo "$output" | jq -r '.tx.txid')
+hash=$(echo "$output" | jq -r '.tx.hash')
+
+# Output txid and hash
+# echo "Transaction ID (txid): $txid"
+echo "$hash"
+
